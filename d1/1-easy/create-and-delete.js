2c12287c969c74810ccbfb9318cf481dfd7199c2
Scott Robinson <scott.robinson2@sky.uk>
2022-10-31 14:11:50 +0000

    Complete d1-easy tasks

--- d1/1-easy/create-and-delete.js
+++ d1/1-easy/create-and-delete.js
@@ -2,11 +2,18 @@ const { db } = require('../../db/db');
 //Create a table called Users with the fields of name and role. Both are text fields.
 
 async function createTable() {
-  await db.query();
+    await db.query(`
+        CREATE TABLE IF NOT EXISTS Users(
+            name TEXT,
+            role TEXT
+        );
+    `);
 }
 //Write a function that will delete the Users table.
 async function deleteTable() {
-  await db.query();
+    await db.query(`
+        DROP TABLE IF EXISTS Users;
+    `);
 }
 
 module.exports = { createTable, deleteTable };
