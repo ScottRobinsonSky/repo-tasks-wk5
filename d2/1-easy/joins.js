8ac3e80c0a9192f900560074095583ffb53a0329
Jordan <43437534+Jaardy@users.noreply.github.com>
2022-11-01 13:51:36 +0000

    Add d2 task templates

---
+++ d2/1-easy/joins.js
@@ -0,0 +1,33 @@
+// const { seed } = require('../db/seedFn');
+
+// const main = async () => {
+//   await seed();
+//   console.log('Seeded');
+// };
+
+// main();
+
+// You know the database now. What can you do with it? Look at the name of the functions provided: what query would give you the desired columns?
+
+const { db } = require('../db/db');
+
+async function employeeNameAndJobTitle() {
+  const test = await db.query(``);
+  console.log(test);
+}
+
+employeeNameAndJobTitle();
+
+async function employeeOS() {
+  const test = await db.query(``);
+  console.log(test);
+}
+
+employeeOS();
+
+async function employeeNameAndPreviousJobTitles() {
+  const test = await db.query(``);
+  console.log(test);
+}
+
+employeeNameAndPreviousJobTitles();
